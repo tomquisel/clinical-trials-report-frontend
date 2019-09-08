897011d65d099faa48e060de9e43047a0c0c390a
Tom Quisel <tom.quisel@gmail.com>
2019-09-07 19:04:16 -0700

    Add file

---
+++ src/components/Site.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "antd/es/button";
+
+import "App.css";
+
+function Site() {
+  return (
+    <div>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default Site;
