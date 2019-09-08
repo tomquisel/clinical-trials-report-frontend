f9b631d824015051fd3fa860d9ace433b620ce95
Tom Quisel <tom.quisel@gmail.com>
2019-09-07 19:11:50 -0700

    Wrap everything in basic antd layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import defaults from "graphql/defaults";
 import resolvers from "graphql/resolvers";
 import typeDefs from "graphql/typeDefs";
 import React from "react";
-import Example from "components/Example";
 import Site from "components/Site";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
@@ -21,7 +20,6 @@ const App = () => (
   <ApolloProvider client={client}>
     <div className="App">
       <Site />
-      <Example />
     </div>
   </ApolloProvider>
 );
