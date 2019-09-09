15dcf151e304000e131f3cc4b0effec2dd3fa9d7
Tom Quisel <tom.quisel@gmail.com>
2019-09-08 18:17:46 -0700

    Merge pull request #4 from tomquisel/tq/add_institutions_demo
    Add demo fetching institutions from client graphql

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -1,11 +1,21 @@
 export default `
   type Query {
     counter: Int
+    institutions: [Institution!]!
   }
+
   type Mutation {
     decrementCounter(): Int
     decrementCounter3(amount: Int): Int
     incrementCounter(): Int
     incrementCounter3(amount: Int): Int
+  }
+
+  type Institution {
+    name: String!
+    trials: Int!
+    ontime: Float!
+    late: Float!
+    missing: Float!
   }
 `;
--- src/pages/Institutions.tsx
+++ src/pages/Institutions.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Button } from "antd";
+import InstitutionsTable from "components/InstitutionsTable";
 
 function Institutions() {
   return (
     <div>
-      <p>Institutions</p>
-      <Button type="primary">Button</Button>
+      <h1>Institutions</h1>
+      <Button type="primary">Button (I do nothing)</Button>
+      <InstitutionsTable />
     </div>
   );
 }
