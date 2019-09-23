e22612c597b10d3fe1d3f6ea310892339c04afa2
Tom Quisel <tom.quisel@gmail.com>
2019-09-22 22:30:33 -0700

    Improve description text

--- src/components/Site.tsx
+++ src/components/Site.tsx
@@ -1,35 +1,11 @@
 import React from "react";
-import { Layout, Menu } from "antd";
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  withRouter,
-} from "react-router-dom";
-import Home from "pages/Home";
+import { Layout } from "antd";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "pages/About";
 import Institutions from "pages/Institutions";
+import Nav from "components/Nav";
 
 const { Header, Content, Footer } = Layout;
-
-function Nav({ location }: { location: { pathname: string } }) {
-  return (
-    <Menu
-      theme="dark"
-      mode="horizontal"
-      style={{ lineHeight: "64px" }}
-      selectedKeys={[location.pathname]}
-    >
-      <Menu.Item key="/">
-        <Link to="/">Home</Link>
-      </Menu.Item>
-      <Menu.Item key="/institutions">
-        <Link to="/institutions">Institutions</Link>
-      </Menu.Item>
-    </Menu>
-  );
-}
-
-const RoutedNav = withRouter(Nav);
 
 function Site() {
   return (
@@ -37,11 +13,11 @@ function Site() {
       <Layout className="layout">
         <Header>
           <div className="logo" />
-          <RoutedNav />
+          <Nav />
         </Header>
         <Content style={{ padding: "0 50px" }}>
           <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
-            <Route exact={true} path="/" component={Home} />
+            <Route exact={true} path="/" component={About} />
             <Route path="/institutions" component={Institutions} />
           </div>
         </Content>
