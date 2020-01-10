9536e6c8d42e4a87a4a30e70eed9f147e39c1575
dstuck <david.e.stuck@gmail.com>
2020-01-09 19:51:22 -0600

    Quick clean up

--- src/pages/Organizations.tsx
+++ src/pages/Organizations.tsx
@@ -6,7 +6,6 @@ function Organizations() {
   return (
     <div>
       <h1>Organizations</h1>
-      <Button type="primary">Button (I do nothing)</Button>
       <OrganizationsTable />
     </div>
   );
