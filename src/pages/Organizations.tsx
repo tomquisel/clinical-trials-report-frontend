89e6e7ab6ae650aa85aa9b0ec9ea8e6aaaefb9b9
David Stück <dstuck@users.noreply.github.com>
2020-01-12 11:01:22 -0600

    Improve table usability (#9)
    * Quick clean up
    
    * Add sortability and fixed filters by trial count

--- src/pages/Organizations.tsx
+++ src/pages/Organizations.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { Button } from "antd";
 import OrganizationsTable from "components/OrganizationsTable";
 
 function Organizations() {
   return (
     <div>
       <h1>Organizations</h1>
-      <Button type="primary">Button (I do nothing)</Button>
       <OrganizationsTable />
     </div>
   );
